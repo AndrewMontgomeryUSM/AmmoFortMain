2fecf2de440827bad149bf8875a21cfa0422b17b
AndrewM87 <andrewmontgomery87@icloud.com>
2025-05-16 11:04:36 +0700

    Function added

--- AmmoFort.cpp
+++ AmmoFort.cpp
@@ -26,7 +26,7 @@ void receiveSupply(int&, Munition*&);
 int main()
 {
     int totalBins = binCounter();
-    Provision* pantry = loadInventory(totalBins);
+    Munition* stock = loadInventory(totalBins);
 
     int choice;
     std::string grocery;   
@@ -48,34 +48,34 @@ int main()
             fillInventory();
             break;
         case 2: "Completing Shopping List:\n";
-            groceryList(totalBins, pantry);
+            groceryList(totalBins, stock);
             break;
         case 3: "Search for item\n"; 
             cout << "Item Name: ";
                 cin.ignore();
                 getline(cin, grocery);
-            if (inList(totalBins, pantry, grocery) == true)
+            if (inList(totalBins, stock, grocery) == true)
                 cout << "Item in stock:\n";
             else
                 cout << "Item Not Found\n";
             break;
         case 4: "Adjusting Quantity on Hand:\n";
-            editQuantity(totalBins, pantry);
-            shoppingList(totalBins, pantry);
+            editQuantity(totalBins, stock);
+            shoppingList(totalBins, stock);
             break;
         case 5: "Adjusting Unit Cost Per Round:\n";
-            editPrice(totalBins, pantry);
+            editPrice(totalBins, stock);
             break;
         case 6: "Receive Order:\n";
-            receiveSupply(totalBins, pantry);
-            shoppingList(totalBins, pantry);
+            receiveSupply(totalBins, stock);
+            shoppingList(totalBins, stock);
             break;
         default:
             cout << "Invalid Selection!\n";
     }
 
-    delete [] pantry;
-    pantry = nullptr;
+    delete [] stock;
+    stock = nullptr;
     return 0;
 }
 
@@ -137,4 +137,52 @@ void fillInventory()
     outFile.close();
 
     cout << "Ammunition Fort Inventory has been saved to <AmmoFort.csv>!\n";
+}
+
+/*
+-----------------------------------------------------------------------------
+Loads inventory from the .csv file into an array for handling withing the 
+program. 
+----------------------------------------------------------------------------- 
+*/
+
+Munition* loadInventory(int& totalBins)
+{   
+    totalBins = binCounter();
+    std::string line;
+
+    // Creates the file if it doesn't exist, replaces it if it does.
+    std::ifstream inFile("pantry.csv");
+
+    if (!inFile)
+    {
+        std::cerr << "Error opening pantry.csv!\n";
+        return nullptr;
+    }
+
+    inFile.clear();                 // clear EOF flag
+    inFile.seekg(0);                // rewind to beginning
+
+    Munition* stock = new Munition[totalBins];
+
+    // I had an easier time going into tempVariables than straight into the object
+    std::string tempName;
+    int tempQuant;
+    double tempCost;
+
+    for (int count = 0; count < totalBins; count++)
+    {
+        getline(inFile, tempName, ',');
+        inFile >> tempQuant;
+        inFile.ignore(); // skip comma
+        inFile >> tempCost;
+        inFile.ignore(); // skip newline
+
+        stock[count].setCal(tempName);
+        stock[count].setQuant(tempQuant);
+        stock[count].setCost(tempCost);
+    }
+
+    inFile.close();
+    return stock; 
 }
